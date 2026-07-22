9c29414bc6a3a797f50b2689225f6b4896bb5553
Sunanda Pamarthi <b24ee1051@iitj.ac.in>
2026-07-23 02:50:23 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+vector<vector<int>> ans;
+vector<int >current;
+
+    void bt(int index, vector<int> nums){
+        if(index==nums.size()){
+            ans.push_back(current);
+            return ;
+        }
+
+        current.push_back(nums[index]);
+        bt(index+1, nums);
+        current.pop_back();
+
+        bt(index+1, nums);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int index=0;
+        bt(0, nums);
+        return ans;
+    }
+};
