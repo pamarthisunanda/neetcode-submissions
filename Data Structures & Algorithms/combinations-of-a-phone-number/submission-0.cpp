6cfc62e6e0e1b78a7732d0be6bd862ba3be4d70b
Sunanda Pamarthi <b24ee1051@iitj.ac.in>
2026-07-24 15:09:12 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+vector<vector <string>> ans;
+string curr;
+
+vector<string> mp={
+    "",
+    "",
+    "abc",
+    "def",
+    "ghi",
+    "jkl",
+    "mno",
+    "pqrs",
+    "tuv",
+    "wxyz"
+
+};
+
+void dfs(int index, string &digits){
+    if(index==digits.size()){
+        ans.push_back(curr);
+        return;
+    }
+
+    string letters= mp[digits[index]-'0'];
+
+    for(char ch: letters){
+        current.push_back(ch);
+        dfs(index+1, digits);
+        current.pop_back();
+    }
+}
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()){
+            return {};
+        }
+
+        dfs(0, digits);
+        return ans;
+    }
+};
