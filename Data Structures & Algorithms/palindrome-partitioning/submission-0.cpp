86a5788b70b3045d134b4d2d4ae4cfd2cf3eb3ce
Sunanda Pamarthi <b24ee1051@iitj.ac.in>
2026-07-24 14:18:40 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+vector<vector<string>> ans;
+
+bool ispal(string &s, int left, int right){
+    while(left<right){
+        if(s[left]!=s[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+
+void dfs(int start, vector<string> & curr ){
+    if(start==s.size()){
+        ans.push_back(curr);
+        return;
+    }
+
+    for(int end=start; end<s.size(); end++){
+        string part = s.substr(start, end - start + 1);
+        if (ispal(part, start, end)) {
+    current.push_back(part);
+    dfs(end + 1);
+    current.pop_back();
+}
+
+    }
+}
+
+    vector<vector<string>> partition(string s) {
+        dfs(0, s);
+        return ans;
+    }
+};
