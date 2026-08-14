a07662b96e507d53cefc4c61d42700d47a7be1ed
Sunanda Pamarthi <b24ee1051@iitj.ac.in>
2026-08-14 21:12:50 +0530

    Add: verifying-an-alien-dictionary - submission-5

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-5.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        
+        vector<int> rank(26);
+
+        for(int i=0; i<26; i++){
+            rank[order[i]-'a']=i;
+        }
+
+        for(int i=0; i<words.size(); i++){
+            string words1=words[i];
+            string words2=words[i+1];
+
+            if(words1[i]==words2[i]){
+                continue;
+            }
+
+            else if(words1[[i]-'a']<words2[[i]-'a']){
+                continue;
+            }
+
+            else{
+                return false;
+            }
+
+
+        for(int j=0; j<min(words1.size(), words2.size()); j++){
+
+            if(words1.size()>words2.size()){
+                return false;
+            }
+
+        
+        }
+
+        }
+
+
+        return true;
+    }
+};
