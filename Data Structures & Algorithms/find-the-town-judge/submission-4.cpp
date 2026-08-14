fff267fcc157ce9ca36725cf1aae9b6d47151798
Sunanda Pamarthi <b24ee1051@iitj.ac.in>
2026-08-15 00:16:58 +0530

    Add: find-the-town-judge - submission-4

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-4.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> t;
+        for(int i=0; i<trust.size() ; i++){
+            t.push_back(trust[i][0]);
+        }
+
+        for(int i=0; i<trust.size(); i++){
+            int c=trust[i][1];
+
+            int count = 0;
+
+for(int j = 0; j < trust.size(); j++) {
+    if(trust[j][1] == c) {
+        count++;
+    }
+
+    if(count == n - 1)
+    return c;
+}
+
+            
+        }
+        else{
+            return -1;
+        }
+        
+    }
+
+    
+};
